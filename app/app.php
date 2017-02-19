0cfc664b5427235c89afd4f2c9e4d42251e93164
kariminus <karim.meciel@gmail.com>
2017-02-19 17:18:53 +0100

    détails d'un article

--- app/app.php
+++ app/app.php
@@ -20,3 +20,8 @@ $app->register(new Silex\Provider\AssetServiceProvider(), array(
 $app['dao.article'] = function ($app) {
     return new Alaska\DAO\ArticleDAO($app['db']);
 };
+$app['dao.comment'] = function ($app) {
+    $commentDAO = new Alaska\DAO\CommentDAO($app['db']);
+    $commentDAO->setArticleDAO($app['dao.article']);
+    return $commentDAO;
+};
