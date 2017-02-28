9464232cbe617cd3c7fb278fa7e74bad6642f31a
kariminus <karim.meciel@gmail.com>
2017-02-28 15:39:23 +0100

    template accueil et article

--- web/lib/jquery/reply.js
+++ web/lib/jquery/reply.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(".reply").click(function(e){
         e.preventDefault();
-        var $form = $('.form');
+        var $form = $('.form-style-7');
         var $this = $(this);
         var parentId = $this.data('id');
         var $comment = $('#comment-' + parentId);
